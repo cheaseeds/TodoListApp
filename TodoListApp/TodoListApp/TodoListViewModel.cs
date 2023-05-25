e85a25b9af350ce6796889cd937a1b74e16606e3
Alexander Chea <58100801+cheaseeds@users.noreply.github.com>
2023-05-25 13:20:08 -0700

    Add project files.

---
+++ TodoListApp/TodoListApp/TodoListViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TodoListApp
+{
+    public class TodoListViewModel
+    {
+        // {get; set;} is to access; modify;
+        //creates a view and updates whenever it updates
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        public TodoListViewModel() 
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+
+        }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public string NewTodoInputValue { get; set; }
+        void AddTodoItem() 
+        {
+        
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+            
+        }
+
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+        void RemoveTodoItem(object o)
+        {
+            TodoItem todoItemBeingRemoved = o as TodoItem;
+            TodoItems.Remove(todoItemBeingRemoved);
+            
+
+        }
+    }
+}
